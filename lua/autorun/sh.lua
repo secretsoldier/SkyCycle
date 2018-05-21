d1dda6e53dc2b8b3423bca7c05fb2efc37702955
secret_survivor <c.mcbrayne@yahoo.co.uk>
2018-05-21 23:13:31 +0100

    Intial Code
    This code moves the sun from one side of the map to the other in one cycle. Supports the sun looking like the moon.. sort of.

---
+++ lua/autorun/sh.lua
@@ -0,0 +1,38 @@
+if SERVER then
+local sun,color = ents.FindByClass("env_sun")[1],"249 249 249"
+sun:SetKeyValue("size",30)
+sun:SetKeyValue("overlaysize",30)
+--sun:SetKeyValue("suncolor",color)
+--sun:SetKeyValue("overlaycolor",color)
+sun:SetKeyValue("sun_dir","-0.1 0 0")
+local time,done = CurTime(),true
+/* while done do
+	print(CurTime()-time)
+	if (CurTime()-time)>1 then
+		done = false
+	end
+end */
+local sun_color,moon_color = string.format("%s %s %s",241,240,199),string.format("%s %s %s",242,242,242)
+function Cycle(enum)
+	if enum == 1 then
+		sun:SetKeyValue("suncolor",moon_color)
+		sun:SetKeyValue("overlaycolor",moon_color)
+		sun:SetKeyValue("HDRColorScale","0.8")
+	elseif enum == 0 or !(enum) then
+		sun:SetKeyValue("suncolor",sun_color)
+		sun:SetKeyValue("overlaycolor",sun_color)
+		sun:SetKeyValue("HDRColorScale","2")
+	end
+	local time,seconds,enable = math.floor(CurTime()),600,true
+	local indent = 2/(seconds*2)
+	function TransFunc()
+		local trans = Lerp(indent*(CurTime()-time),-1,1)
+		sun:SetKeyValue("sun_dir",string.format("%s 0 0",trans))
+		if CurTime()-time < seconds and enable then
+			timer.Simple(0.1,TransFunc)
+		end
+	end
+	timer.Simple(0.1,TransFunc)
+end	
+Cycle()
+end
