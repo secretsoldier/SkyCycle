335527c7a3c802a1f175ca0017c6fb5ad424ebbf
secretsoldier <c.mcbrayne@yahoo.co.uk>
2018-06-17 12:37:21 +0100

    Added ULX Param function
    Helps with creating the parameters for the ULX commands.

--- lua/modules/ulx.lua
+++ lua/modules/ulx.lua
@@ -2,14 +2,13 @@
 local function ulxCommand(name,func) return ulx.command("Sky Cycle",string.format("ulx %s",name),function() SinglePlayerError() func() end,string.format("!%s",name),false,false,false) end
 local function ulxLog(string,ply,arg1,arg2,arg3) ulx.fancyLogAdmin(ply,string.format("#A %s",string.format(string,arg1,arg2,arg3))) end
 local function ulxError(string,ply) ULib.tsayError(ply,string,false) end
-local function SinglePlayerError() if game.SinglePlayer() then Ulib.console(nil,"!Most features of SkyCycle do not work in SinglePlayer!") end
+local function ulxParam(enum,hint,optional,restrict,round) optional = optional or nil restrict = restrict or nil round = round or nil
+	local t = {{type=ULib.cmds.NumArg},{type=ULib.cmds.BoolArg},{type=ULib.cmds.PlayerArg},{type=ULib.cmds.PlayersArg},{type=ULib.cmds.StringArg}} t[enum].hint = hint 
+	if !(t[enum]) then return end if optional then table.Add(t[enum],{ULib.cmds.optional}) end if restrict then table.Add(t[enum],{ULib.cmds.restrictToCompletes}) end
+	if round then table.Add(t[enum],{ULib.cmds.round}) end return t[enum] end
+local NUM,BOOL,PLAYER,PLAYERS,STRING = 1,2,3,4,5 -- Proper enums
+local function SinglePlayerError() if game.SinglePlayer() then ULib.console(nil,"!Most features of SkyCycle do not work in SinglePlayer!") end
 local USER,OPERATOR,ADMIN,SUPERADMIN = "user","operator","admin","superadmin" -- They must of never heard of enums...
-local function ulxTable(t,hint)
-	return {type=ULib.cmds.StringArg,completes=t,hint=hint,ULib.cmds.restrictToCompletes}
-end
-local function ulxPercentBar(t,hint)
-	return {type=ULib.cmds.NumArg,default=t.default,min=t.min,max=t.max,hint=hint,ULib.cmds.optional}
-end
 
 local force_cycle = ulxCommand("Force Cycle",function(ply) RunCycle() ulxLog("Forced a Day Cycle",ply) end)
 force_cycle:help"Forces a day cycle."
