939b7088855ccc06c366201735585b6655ec2e90
secret_survivor <c.mcbrayne@yahoo.co.uk>
2018-06-25 20:12:45 +0100

    Update
    Forgot to push this latest commit.

--- lua/autorun/sh.lua
+++ lua/autorun/sh.lua
@@ -10,22 +10,48 @@ if SERVER then
 			end
 		end
 
-		local enable,seconds = true,600
+		local cycle_enable,seconds = true,600
 		function RunCycle(offset)
-			enable = true offset = offset or 0
+			cycle_enable = true offset = offset or 0
 			local time,indent = math.floor(CurTime()),2/(seconds*2)
 			function TransFunc()
 				hook.Run("SkyCycle_Timer",Lerp(indent*((CurTime()+offset)-time),1,-1))
-				if CurTime()-time < seconds and enable then timer.Simple(0.1,TransFunc) end
+				if CurTime()-time < seconds and cycle_enable then timer.Simple(0.1,TransFunc) end
 			end
 			timer.Simple(0.1,TransFunc)
 		end
-		function EnableCycleTimer(b) enable = b end
+		function EnableCycleTimer(b) cycle_enable = b end
 		function SetCycleLength(secs) seconds = secs end
-		function Percent(a,b) return (a/b)*100 end
+		function Percent(a) return 1-((a+1)/2) end
+
+		local time_enable,day = true,true
+		local time = 0
+		local time_table = 
+		{
+			{"Sun Rise",6,9,},
+			{"Day",9,20,},
+			{"Sun Set",20,22,},
+			{"Night",22,6,}
+		}
+		function ReturnTime() return time,day end
+		hook.Add("SkyCycle_Timer","SkyCycle_Time",function(per)
+			time = math.floor(12*Percent(per))+6
+			if !(day) then time = time + 12 end
+			if time > 24 then time = time - 24 end
+
+
+		end)
+		local cyclehopper_enable = true
+		hook.Add("SkyCycle_Timer","SkyCycle_CycleHopper",function(per)
+			if (per == -1) and cyclehopper_enable then
+				day = !day
+				RunCycle()
+			end
+		end)
+		function EnableCycleHopper(bool) cyclehopper_enable = bool end
 		
 		local sun,color = ents.FindByClass("env_sun")[1],"249 249 249"
-		if !(sun) then ErrorNoHalt("env_sun isn't present in the map.") return end 
+		if !(sun) then Error("env_sun isn't present in the map.") return end 
 		sun:SetKeyValue("size",30)
 		sun:SetKeyValue("overlaysize",30)
 		local sun_color,moon_color = string.format("%s %s %s",241,240,199),string.format("%s %s %s",242,242,242)
@@ -39,23 +65,28 @@ if SERVER then
 			sun:SetKeyValue("sun_dir",string.format("%s 0 0",dir))
 		end)
 
-
 		local sky = ents.FindByClass("env_skypaint")[1]
+		local starfield,clouds = "skybox/clouds","skybox/starfield"
 		sky:SetupDataTables()
-		if !(sky) then ErrorNoHalt("env_skypaint isn't present in the map") end
+		sky:SetDrawStars(true)
+		if !(sky) then Error("env_skypaint isn't present in the map") end
 		function RGB_to_color_vec(r,g,b)
 			return Vector(r/255,g/255,b/255)
-		end
-		function Night()
-			sky:SetTopColor(Vector(0,0,0))
-			sky:SetBottomColor(Vector(0,0,0.04))
 		end
 		--Top Color: 0 0 0
 		--Bottom Color: 0 0 0.04
 		-- Star Scale: 1.28
 
-		
+		function Day()
+			sky:SetTopColor(Vector(0.2,0.5,1.0))
+			sky:SetBottomColor(Vector(0.8,1.0,1.0))
 			
+		end
+		function Night()
+			sky:SetTopColor(Vector(0,0,0))
+			sky:SetBottomColor(Vector(0,0,0.04))
+			
+		end
 		if !(ulx) then
 			concommand.Add("SingleCycle",function() RunCycle() end)
 			concommand.Add("SetSunSize",function(ply,cmd,args,argStr) if!(args[1])then print("This command requires an Argument.")return else sun:SetKeyValue("size",args[1])end end)
--- lua/ulx/modules/sh/ulx.lua
+++ lua/ulx/modules/sh/ulx.lua
@@ -2,15 +2,14 @@ AddCSLuaFile()
 local function ulxCommand(name,func) return ulx.command("SkyCycle",string.format("ulx %s",name),func,string.format("!%s",name),false,false,false) end
 local function ulxLog(string,ply,arg1,arg2,arg3) ulx.fancyLogAdmin(ply,string.format("#A %s",string.format(string,arg1,arg2,arg3))) end
 local function ulxError(string,ply) ULib.tsayError(ply,string,false) end
-local function ulxParam(enum,hint,optional,restrict,round) optional = optional or nil restrict = restrict or nil round = round or nil
+local function ulxParam(enum,hint,optional,restrict,round,restofline) optional = optional or nil restrict = restrict or nil round = round or nil restofline = restofline or nil
 	local t = {{type=ULib.cmds.NumArg},{type=ULib.cmds.BoolArg},{type=ULib.cmds.PlayerArg},{type=ULib.cmds.PlayersArg},{type=ULib.cmds.StringArg}} t[enum].hint = hint 
 	if !(t[enum]) then return end if optional then table.Add(t[enum],{ULib.cmds.optional}) end if restrict then table.Add(t[enum],{ULib.cmds.restrictToCompletes}) end
-	if round then table.Add(t[enum],{ULib.cmds.round}) end return t[enum] end
+	if restofline then table.Add(t[enum],{ULib.cmds.takeRestOfLine}) end if round then table.Add(t[enum],{ULib.cmds.round}) end return t[enum] end
 local NUM,BOOL,PLAYER,PLAYERS,STRING = 1,2,3,4,5 -- Proper enums
-local function SinglePlayerError() if game.SinglePlayer() then ULib.console(nil,"!Most features of SkyCycle do not work in SinglePlayer!") end end
-local USER,OPERATOR,ADMIN,SUPERADMIN = "user","operator","admin","superadmin" -- They must of never heard of enums...
+local ALL,OPERATOR,ADMIN,SUPERADMIN = "user","operator","admin","superadmin" -- They must of never heard of enums...
 
-local force_cycle = ulxCommand("forcecycle",function(ply) RunCycle() ulxLog("Forced a Cycle",ply) end)
+local force_cycle = ulxCommand("forcecycle",function(ply) EnableCycleHopper(false) RunCycle() ulxLog("Forced a Cycle",ply) end)
 force_cycle:help"Forces a cycle."
 force_cycle:defaultAccess(ADMIN)
 force_cycle:addParam{type=ULib.cmds.BoolArg, invisible=true}
@@ -22,13 +21,16 @@ local disable_cycle_param = ulxParam(BOOL)
 disable_cycle_param.invisible = true
 force_cycle:addParam(disable_cycle_param)
 
-local cycle_length = ulxCommand("setcyclelength",function(ply,seconds) SetCycleLength(seconds) ulxLog("Set the Cycle Length to %s seconds.",ply,seconds) end) -- Will be removed in a later date.
-cycle_length:help"Sets the amount of time it takes for a whole cycle to pass in seconds."
-cycle_length:addParam{type=ULib.cmds.NumArg,default=600,min=60,hint="seconds",ULib.cmds.round}
+local cycle_length = ulxCommand("setcyclelength",function(ply,minutes) SetCycleLength(minutes*60) ulxLog("Set the Cycle Length to %s minutes.",ply,minutes) end) -- Will be removed in a later date.
+cycle_length:help"Sets the amount of time it takes for a whole cycle to pass in minutes."
 cycle_length:defaultAccess(SUPERADMIN)
-local cycle_length_param = ulxParam(BOOL)
-cycle_length_param.invisible = true
-cycle_length:addParam(cycle_length_param)
+local cycle_length_param1 = ulxParam(NUM,"minutes",false,false,true)
+cycle_length_param1.default = 10
+cycle_length_param1.min = 1
+cycle_length:addParam(cycle_length_param1)
+local cycle_length_param2 = ulxParam(BOOL)
+cycle_length_param2.invisible = true
+cycle_length:addParam(cycle_length_param2)
 
 local night_sky = ulxCommand("nightsky",function(ply) Night() ulxLog("Made it night.",ply) end)
 night_sky:help"Test command to make the sky night."
@@ -36,3 +38,21 @@ night_sky:defaultAccess(ADMIN)
 local night_sky_param = ulxParam(BOOL)
 night_sky_param.invisible = true
 night_sky:addParam(night_sky_param)
+
+local day_sky = ulxCommand("daysky",function(ply) Day() ulxLog("Made it day.",ply) end)
+day_sky:help"Test command to make the sky day."
+day_sky:defaultAccess(ADMIN)
+local day_sky_param = ulxParam(BOOL)
+day_sky_param.invisible = true
+day_sky:addParam(day_sky_param)
+
+local get_time = ulxCommand("gettime",function(ply,format) local x,z = ReturnTime() if format == "24 Hour" then  else if !(z) then if x > 12 then x=x-12 end x = string.format("%s pm",x) else x = string.format("%s am",x) end end ULib.tsay(ply,string.format("The time is: %s",x)) end)
+get_time:help"Command that gets the time in-game."
+get_time:defaultAccess(ALL)
+local get_time_param1 = ulxParam(STRING,"Time Format",true,true,false,true)
+get_time_param1.completes = {"24 Hour","AM-PM"} -- Format doesn't work properly right now.
+get_time_param1.error = "Please choose a format"
+get_time:addParam(get_time_param1)
+local get_time_param2 = ulxParam(BOOL)
+get_time_param2.invisible = true
+get_time:addParam(get_time_param2)
